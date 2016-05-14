c51dbffd3cd90406e3d84bba1af43cddb3c9f54f
meed <meed2000@gmail.com>
2016-05-14 23:10:02 +0800

    Initial commit

---
+++ History.md
@@ -0,0 +1,5 @@
+
+1.0.0 / 2016-05-14
+==================
+
+* wraps koa-multer for koa-router
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/*!
+ * multer
+ * Copyright(c) 2014 Hage Yaapa
+ * Copyright(c) 2015 Fangdun Cai
+ * Copyright(c) 2016 Hirako2000
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+const multerFields = ['file', 'files', 'body'];
+const originalMulter = require('multer');
+
+function multer(options) {
+  const m = originalMulter(options);
+
+  const _makeMiddleware = m._makeMiddleware.bind(m);
+  m._makeMiddleware = makePromise(_makeMiddleware);
+
+  const any = m.any.bind(m);
+  m.any = makePromise(any);
+
+  return m
+}
+
+function makePromise(fn) {
+  return (fields, fileStrategy) => {
+    return function *(next) {
+      yield promise(this);
+      yield next;
+    };
+    function promise(ctx) {
+      return new Promise((resolve, reject) => {
+        fn(fields, fileStrategy)(ctx.req, ctx.res, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            copyFields(ctx);
+            resolve(ctx)
+          }
+        })
+      });
+    }
+  }
+}
+
+function copyFields(ctx) {
+  let req = ctx.req;
+  let len = multerFields.length;
+  let field;
+  for (let i = 0; i < len; i ++) {
+    field = multerFields[i];
+    if (req.hasOwnProperty(field)) {
+      ctx[field] = req[field];
+    }
+  }
+}
+
+multer.diskStorage = originalMulter.diskStorage;
+multer.memoryStorage = originalMulter.memoryStorage;
+
+exports = module.exports = multer;
